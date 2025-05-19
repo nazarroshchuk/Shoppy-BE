0467abe850167d462cc03f600da318f05311fcc7
nazarii.roshchuk <nazarii.roshchuk@gmail.com>
2025-05-19 20:25:16 +0200

    added image Upload API

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProductRequest } from './dto/create-product.request';
 import { PrismaService } from '../prisma/prisma.service';
+import { promises as fs } from 'fs';
+import { join } from 'path';
 
 @Injectable()
 export class ProductsService {
@@ -16,6 +18,25 @@ export class ProductsService {
   }
 
   async getProducts() {
-    return this.prismaService.product.findMany();
+    const products = await this.prismaService.product.findMany();
+    return Promise.all(
+      products.map(async (product) => ({
+        ...product,
+        imageExists: await this.imageExists(product.id),
+      })),
+    );
+  }
+
+  private async imageExists(id: number) {
+    try {
+      // it throws an error when file does not exist in the directory
+      await fs.access(
+        join(__dirname, '../../', `public/products/${id}.jpeg`),
+        fs.constants.F_OK,
+      );
+      return true;
+    } catch {
+      return false;
+    }
   }
 }
